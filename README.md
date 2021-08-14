71d7350391b7e5d3a7d2ed7a2891120eb6ebb94f
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2021-08-14 14:40:02 +0300

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Tip-Calculator
+
+iOS's application for calculating a particular percent of tips of the total price received in receipt.
+
+## PROJECT SPECIFICATION
+
+• Operating system: [iOS](https://www.apple.com/ios/ios-14/);
+
+• Programming language: [Swift](https://developer.apple.com/swift/);
+
+• SDK: [iOS](https://developer.apple.com/);
+
+•  Interface: [SwiftUI](https://developer.apple.com/xcode/swiftui/).
--- Tip Calculator/ContentView.swift
+++ Tip Calculator/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var total: String = "20"
+    @State var tipPercent: Double = 15.0
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Tip Calculator")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            HStack{
+                Text("$")
+                TextField("Total", text: $total)
+                    .border(Color.black,width: 0.25)
+            }
             .padding()
+            
+            HStack {
+                Slider(value: $tipPercent, in: 1...30, step: 1.0)
+                Text("\(Int(tipPercent))%")
+            }
+            .padding()
+            
+            if let totalNum = Double(total){
+                Text("Tip Amount: $\(totalNum * tipPercent / 100, specifier:"%0.2f" )")
+            } else {
+                Text("Please eneter a valid numerical value.")
+            }
+        }
     }
 }
 
