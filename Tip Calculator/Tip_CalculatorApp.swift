9062f0a8e2117f2b34f9009957280fc423c662c6
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2021-08-14 10:33:39 +0300

    Initial Commit

---
+++ Tip Calculator/Tip_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalculatorApp.swift
+//  Tip Calculator
+//
+//  Created by Dmytro on 14.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct Tip_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
